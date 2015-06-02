52765b824ba8a5eb78c1afd9c427393677ccf26f
Craig Jellick <craig@rancher.com>
2015-06-02 08:30:56 -0700

    Consolidate proxy, backend, client to one project

---
+++ backend/backend.go
@@ -0,0 +1,107 @@
+package backend
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/websocket"
+)
+
+var responders = make(map[string]chan string)
+
+type Handler interface {
+	Handle(string, <-chan string, chan<- *MessageWrapper)
+}
+
+type MessageWrapper struct {
+	Key     string
+	Message string
+}
+
+func ConnectToProxy(proxyUrl string, handlers map[string]Handler) {
+	log.WithFields(log.Fields{
+		"url": proxyUrl,
+	}).Info("Connecting to proxy.")
+
+	dialer := &websocket.Dialer{}
+	headers := http.Header{}
+	headers.Add("X-Cattle-HostId", "1")
+	ws, _, err := dialer.Dial(proxyUrl, headers)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Failed to connect to proxy.")
+	}
+
+	responseChannel := make(chan *MessageWrapper, 10)
+
+	go func() {
+		for {
+			wrap := <-responseChannel
+			data := fmt.Sprintf("%s||%s", wrap.Key, wrap.Message)
+
+			ws.WriteMessage(1, []byte(data))
+		}
+	}()
+
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Warn("Error reading message.")
+			continue
+		}
+
+		parts := strings.SplitN(string(msg), "||", 3)
+		if len(parts) != 3 {
+			continue
+		}
+
+		responderKey := parts[0]
+		msgType, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error":       err,
+				"messageType": parts[1],
+			}).Warn("Error parsing message type.")
+			continue
+		}
+
+		if msgType == 0 {
+			requestUrl, err := url.Parse(parts[2])
+			if err != nil {
+				continue
+			}
+			handler, ok := handlers[requestUrl.Path]
+			if !ok {
+				// TODO Tell proxy we quit
+				log.WithFields(log.Fields{
+					"path": requestUrl.Path,
+				}).Warn("Could not find appropriate message handler for supplied path.")
+				continue
+			}
+			msgChan := make(chan string, 10)
+			responders[responderKey] = msgChan
+			go handler.Handle(responderKey, msgChan, responseChannel)
+
+		} else if msgType == 8 {
+			// Using 8 because it parallels the number assigned to close message types
+			// in the websocket protocal, but it's arbitrary.
+			if msgChan, ok := responders[responderKey]; ok {
+				close(msgChan)
+			}
+		} else {
+			if msgChan, ok := responders[responderKey]; ok {
+				msgString := parts[2]
+				msgChan <- msgString
+			} else {
+				// TODO Coudln't find reply channel. Tell proxy we quite.
+			}
+		}
+	}
+}
---
+++ proxy/integration_test.go
@@ -0,0 +1,120 @@
+package proxy
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
+
+	"github.com/rancherio/websocket-proxy/backend"
+)
+
+func TestEndToEnd(t *testing.T) {
+	t.Logf("Starting test...")
+	go StartProxy()
+
+	handlers := make(map[string]backend.Handler)
+	handlers["/v1/echo"] = &EchoHandler{}
+
+	go backend.ConnectToProxy("ws://localhost:1111/connectbackend", handlers)
+	time.Sleep(300 * time.Millisecond)
+
+	url := "ws://localhost:1111/v1/echo?hostId=1"
+	t.Logf("Connecting URL: %s\n", url)
+	dialer := &websocket.Dialer{}
+	headers := http.Header{}
+	ws, _, err := dialer.Dial(url, headers)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sendAndAssertReply(ws, strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10), t)
+	time.Sleep(1 * time.Millisecond) // Ensure different timestamp
+	sendAndAssertReply(ws, strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10), t)
+
+}
+
+func sendAndAssertReply(ws *websocket.Conn, msg string, t *testing.T) {
+	message := []byte(msg)
+	err := ws.WriteMessage(1, message)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Sent: %s\n", message)
+
+	_, reply, err := ws.ReadMessage()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Received: %s\n", reply)
+	if msg+"-response" != string(reply) {
+		t.Fatalf("Unexpected repsonse: [%v]", reply)
+	}
+}
+
+type EchoHandler struct {
+}
+
+func (e *EchoHandler) Handle(key string, incomingMessages <-chan string, response chan<- *backend.MessageWrapper) {
+	for {
+		m, ok := <-incomingMessages
+		if !ok {
+			return
+		}
+		data := fmt.Sprintf("%s-response", m)
+		wrap := &backend.MessageWrapper{
+			Key:     key,
+			Message: data,
+		}
+		response <- wrap
+	}
+}
+
+type LogsHandler struct {
+}
+
+func (l *LogsHandler) Handle(key string, incomingMessages <-chan string, response chan<- *backend.MessageWrapper) {
+	idx := 0
+	ticker := time.NewTicker(100 * time.Millisecond)
+	msg := ""
+	for {
+		select {
+		case m, ok := <-incomingMessages:
+			if !ok {
+				return
+			}
+			msg += m
+		case <-ticker.C:
+			if msg == "" {
+				msg = "logs"
+			}
+			data := fmt.Sprintf("%s %d", msg, idx)
+			wrap := &backend.MessageWrapper{
+				Key:     key,
+				Message: data,
+			}
+			response <- wrap
+		}
+		idx++
+	}
+}
+
+/*
+	count := 0
+	for count <= 10 {
+		if count >= 10 && count%10 == 0 {
+			msg := body + " " + strconv.Itoa(count)
+			t.Logf("Sending new message [%s]\n", msg)
+			ws.WriteMessage(1, []byte(msg))
+		}
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Test Receive: %s\n", msg)
+		count++
+	}
+*/
