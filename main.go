f41ba1cecfed019c2d721f75c550ea21c1d38ffb
Darren Shepherd <darren@rancher.com>
2016-08-31 00:10:07 -0700

    Add V2 routes

--- main.go
+++ main.go
@@ -14,13 +14,48 @@ func main() {
 	}
 
 	p := &proxy.Starter{
-		BackendPaths:       []string{"/v1/connectbackend"},
-		FrontendPaths:      []string{"/v1/{logs:logs}/", "/v1/{stats:stats}", "/v1/{stats:stats}/{statsid}", "/v1/exec/", "/v1/console/", "/v1/dockersocket/"},
-		FrontendHTTPPaths:  []string{"/v1/container-proxy{path:.*}", "/r/projects/{project}/{service}{path:.*}", "/r/{service}{path:.*}"},
-		StatsPaths:         []string{"/v1/{hoststats:hoststats(\\/project)?(\\/)?}", "/v1/{containerstats:containerstats(\\/service)?(\\/)?}", "/v1/{containerstats:containerstats}/{containerid}"},
-		CattleWSProxyPaths: []string{"/v1/{sub:subscribe}", "/v1/projects/{project}/{sub:subscribe}"},
-		CattleProxyPaths:   []string{"/{cattle-proxy:.*}"},
-		Config:             conf,
+		BackendPaths: []string{
+			"/v1/connectbackend",
+			"/v2-beta/connectbackend",
+		},
+		FrontendPaths: []string{
+			"/v1/{logs:logs}/",
+			"/v1/{stats:stats}",
+			"/v1/{stats:stats}/{statsid}",
+			"/v1/exec/",
+			"/v1/console/",
+			"/v1/dockersocket/",
+			"/v2-beta/{logs:logs}/",
+			"/v2-beta/{stats:stats}",
+			"/v2-beta/{stats:stats}/{statsid}",
+			"/v2-beta/exec/",
+			"/v2-beta/console/",
+			"/v2-beta/dockersocket/",
+		},
+		FrontendHTTPPaths: []string{
+			"/v1/container-proxy{path:.*}",
+			"/v2-beta/container-proxy{path:.*}",
+			"/r/projects/{project}/{service}{path:.*}",
+			"/r/{service}{path:.*}",
+		},
+		StatsPaths: []string{
+			"/v1/{hoststats:hoststats(\\/project)?(\\/)?}",
+			"/v1/{containerstats:containerstats(\\/service)?(\\/)?}",
+			"/v1/{containerstats:containerstats}/{containerid}",
+			"/v2-beta/{hoststats:hoststats(\\/project)?(\\/)?}",
+			"/v2-beta/{containerstats:containerstats(\\/service)?(\\/)?}",
+			"/v2-beta/{containerstats:containerstats}/{containerid}",
+		},
+		CattleWSProxyPaths: []string{
+			"/v1/{sub:subscribe}",
+			"/v1/projects/{project}/{sub:subscribe}",
+			"/v2-beta/{sub:subscribe}",
+			"/v2-beta/projects/{project}/{sub:subscribe}",
+		},
+		CattleProxyPaths: []string{
+			"/{cattle-proxy:.*}",
+		},
+		Config: conf,
 	}
 
 	log.Infof("Starting websocket proxy. Listening on [%s], Proxying to cattle API at [%s], Monitoring parent pid [%v].",
